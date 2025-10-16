6fb0247fcf48112e20c2b651bcfdccb1fb995552
Rijaa Kukda <rijaa04@gmail.com>
2025-10-16 01:00:03 +0100

    updated

--- sql/sla.sql
+++ sql/sla.sql
@@ -6,6 +6,7 @@ SELECT issue_type,
        COUNT(*) AS calls,
        AVG(CAST(resolution_time AS numeric)) AS avg_resolution_minutes,
        SUM(CASE WHEN LOWER(resolved) = 'yes' THEN 1 ELSE 0 END) AS resolved_count,
+	   SUM(CASE WHEN LOWER(resolved) = 'no' THEN 1 ELSE 0 END) AS unresolved_count,
        ROUND(100.0 * SUM(CASE WHEN LOWER(resolved) = 'yes' THEN 1 ELSE 0 END) / NULLIF(COUNT(*),0),2) AS pct_resolved
 FROM staging.support_calls_raw
 GROUP BY issue_type
--- sql/support_features.sql
+++ sql/support_features.sql
@@ -7,10 +7,11 @@
 DROP TABLE IF EXISTS analytics.support_features;
 CREATE TABLE IF NOT EXISTS analytics.support_features AS
 SELECT
-    customerid,
+    customerid, issue_type, resolved,
     COUNT(*) FILTER (WHERE TO_DATE(call_date, 'MM/DD/YYYY') >= '2023-01-01'::DATE - INTERVAL '90 days') AS calls_last_90d,
     COUNT(*) FILTER (WHERE resolved='No' AND TO_DATE(call_date, 'MM/DD/YYYY') <= '2023-01-01'::DATE - INTERVAL '90 days') AS unresolved_calls_90d,
-    ROUND(AVG(resolution_time::INT),2) AS avg_resolution_time
+    ROUND(AVG(resolution_time::INT),2) AS avg_resolution_time,
+	COUNT(*) AS Resolved_char
 FROM staging.support_calls_raw
-GROUP BY customerid;
+GROUP BY customerid, issue_type, resolved;
 
